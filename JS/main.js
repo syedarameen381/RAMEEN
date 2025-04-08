93ddbc467b0ec515f36a53c86446a1bf0bd787ab
syedarameen381 <syedarmn64@gmail.com>
2025-04-08 10:32:08 +0300

    8th april

---
+++ JS/main.js
@@ -0,0 +1,57 @@
+
+let Abc = "hello world <br>";
+
+Abc = "Hello hira <br>";
+document.write(Abc)
+
+
+var b = 'hello class <br>';
+//var b = "this is redec <br>";
+//b = 'hello saira'
+document.write(b)
+//alert(b)
+
+const c = "this is const"
+///document.write(c)
+console.log (c)
+
+{ 
+ var x ='in a block';//string;
+ var y = 1234;//number
+ var z = 1345798876542n;//bigint
+ var xyz = false; //boolean
+  var f = null//null
+  var un //undefined
+  un=["string", "second", true, 12345];//array
+  var ob = {
+    name:'emma',
+    married:false,
+    age:15,
+    arr:["value1", "value2"]//object
+
+  }
+}
+//arithmatic operators
+var n = 45 + 5;//addition+
+n = 45 - 5;//subtraction-
+n = 45*5;// multiplaction *
+n = 2**3;//exponentiation **
+n = 5/2; //division /
+n = 5%2;//remainder %
+let inc = 5;
+inc++
+let btx = inc//increment ++
+
+let dec = 9;
+dec--
+let abc = dec//decrement --
+console.log(inc)
+document.write("<h1>" + inc +"</h1>")
+
+//assignment operators
+
+
+
+
+
+
