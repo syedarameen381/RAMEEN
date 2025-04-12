70e32684c458769e4055084c5788f7ed0c35ca93
syedarameen381 <syedarmn64@gmail.com>
2025-04-12 22:48:08 +0300

    13 April

--- JS/main.js
+++ JS/main.js
@@ -53,5 +53,63 @@ document.write("<h1>" + inc +"</h1>")
 
 
 
+//array methods 
+
+// let arr = ["green", "red","yellow","orange"];
+//console.log(arr.lenght); lenght
+//console.log(arr.toString()); string
+//console.log(arr[3]); index
+///console.log(arr.at(2)); at
+//document.write(arr.join(".")); join
+///arr.pop(); to remove the last value
+//arr.push("blue"); to add value at the last
+//console.log(arr)
+
+//arr.shift(); remove first value
+//arr.unshift("pink"); add value at first
+//console.log(arr);
+
+//delete arr[2]; remove value from any point but leave an empty space
+//console.log(arr[2]);
+
+// let arr1 = ["sun","moon","star"];
+// let arr2 = ["venus","mars","earth"];
+// let newarr=arr1.concat(arr2);
+// console.log(newarr)
+
+let arr = ["brazil","morocco","uk","ibiza",];
+//console.log(arr.length);
+// console.log(arr.toString());
+//console.log(arr[2]);
+//console.log(arr.at(1));
+// console.log(arr.join("/"));
+// arr.pop();
+// arr.push("usa");
+// arr.shift();
+// arr.unshift("usa");
+//delete arr[1]
+//console.log(arr)
+//let arr1 = ["africa","KSA","Oman"];
+//let newarr = arr.concat(arr1);
+//arr.copyWithin(2,0);
+//console.log(arr);
+//const myArr = [[1,2],[3,4],[5,6]];
+//let flatarr = myArr.flat()
+//console.log(flatarr)
+
+let arr3 = [1,2,3,4,];
+let newarr3=arr3.splice(1,1,"stars","moon")
+console.log(arr3)
+console.log(newarr3)
+const elemnts = ["H","He","Ca","Na","K"];
+//const spliced =elemnts.tospliced[]
+
+
+
+
+
+
+
+
 
 
